7b7bd944884695bdd5e25d890262c7eb7b8009f4
PiotrIzw <piotr.izworski.0@gmail.com>
2019-12-03 14:30:54 +0100

    Single class changes

--- WarehouseSystem/Boss.cpp
+++ WarehouseSystem/Boss.cpp
@@ -1,6 +1,7 @@
 #include "Boss.h"
+#include <string>
 
-void Boss::sendMessage(string message, string receiver) {
+void Boss::sendMessage(std::string message, std::string receiver) {
 	// TODO - implement Boss::sendMessage
 	throw "Not yet implemented";
 }
