30701b74eb7161472a3a0571045e7186a24e5998
PiotrIzw <piotr.izworski.0@gmail.com>
2020-01-11 19:33:45 +0100

    Updated database.db, database class

--- WarehouseSystem/Database.cpp
+++ WarehouseSystem/Database.cpp
@@ -3,27 +3,58 @@
 #include <sqlite3.h>
 #include <string>
 
+int rc;
+char* sql;
+sqlite3* db;
+char* zErrMsg = 0;
+int exit = 0;
+
+static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
+    int i;
+    for (i = 0; i < argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+
 int checkConnection() {
-    sqlite3* DB;
-    int exit = 0;
-    exit = sqlite3_open("warehouse.db", &DB);
+    
+    exit = sqlite3_open("warehouse.db", &db);
 
     if (exit) {
-        std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+        std::cerr << "Error open DB " << sqlite3_errmsg(db) << std::endl;
         return (-1);
     }
     else
         std::cout << "Opened Database Successfully!" << std::endl;
-    sqlite3_close(DB);
+    sqlite3_close(db);
     return (0);
 }
 
-void addEmployerToDB(std::string name, std::string surname, int pesel) { //uzupelnic atrybuty
-    //uzupelnic
+void addEmployerToDB(char* name, char* surname, char* pesel) { //uzupelnic atrybuty
+    
+    char* a = strcpy(sql, name); //test
+    sql << "INSERT INTO COMPANY (id,name,surname,pesel) "  \
+        "VALUES (1, '" << name << "', '" << surname << "', '" << pesel << "'); ";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else {
+        std::cout << "Records created successfully\n");
+    }
+    sqlite3_close(db);
 }
 
 void addClientToDB(std::string name, std::string surname, int pesel) { //uzupelnic atrybuty
     //uzupelnic
 }
 
+void 
 
