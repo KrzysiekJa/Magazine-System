0cf60bc103ed33cc8a015849cdd0b46f7aaf2e41
PiotrIzw <piotr.izworski.0@gmail.com>
2020-01-12 15:14:34 +0100

    Updated warehouse.db, database class

--- WarehouseSystem/Database.cpp
+++ WarehouseSystem/Database.cpp
@@ -1,13 +1,29 @@
 #include "Database.h"
 #include <iostream> 
-#include <sqlite3.h>
+#include <stdio.h>
+#include <sqlite3.h> 
 #include <string>
 
-int rc;
-char* sql;
 sqlite3* db;
 char* zErrMsg = 0;
-int exit = 0;
+int rc;
+std::string sql_string;
+const char* sql;
+
+int checkConnection() {
+    //tylko do sprawdzania polaczania
+
+    rc = sqlite3_open("warehouse.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    }
+    else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    sqlite3_close(db);
+}
 
 static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
     int i;
@@ -18,26 +34,24 @@ static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
     return 0;
 }
 
+int createTable() {
+    //uzyc tylko raz do tworzenia tabeli
+    rc = sqlite3_open("warehouse.db", &db);
 
-int checkConnection() {
-    
-    exit = sqlite3_open("warehouse.db", &db);
-
-    if (exit) {
-        std::cerr << "Error open DB " << sqlite3_errmsg(db) << std::endl;
-        return (-1);
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
     }
-    else
-        std::cout << "Opened Database Successfully!" << std::endl;
-    sqlite3_close(db);
-    return (0);
-}
+    else {
+        fprintf(stdout, "Opened database successfully\n");
+    }
 
-void addEmployerToDB(char* name, char* surname, char* pesel) { //uzupelnic atrybuty
-    
-    char* a = strcpy(sql, name); //test
-    sql << "INSERT INTO COMPANY (id,name,surname,pesel) "  \
-        "VALUES (1, '" << name << "', '" << surname << "', '" << pesel << "'); ";
+    /* Create SQL statement */
+    sql = "CREATE TABLE EMPLOYERS("  \
+        "ID INTEGER PRIMARY KEY     NOT NULL," \
+        "NAME           TEXT    NOT NULL," \
+        "SURNAME        TEXT    NOT NULL," \
+        "PESEL          INTEGER     NOT NULL);";
 
     /* Execute SQL statement */
     rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
@@ -47,14 +61,90 @@ void addEmployerToDB(char* name, char* surname, char* pesel) { //uzupelnic atryb
         sqlite3_free(zErrMsg);
     }
     else {
-        std::cout << "Records created successfully\n");
+        fprintf(stdout, "Table created successfully\n");
     }
     sqlite3_close(db);
 }
 
-void addClientToDB(std::string name, std::string surname, int pesel) { //uzupelnic atrybuty
-    //uzupelnic
+
+void addEmployerToDB(std::string name, std::string surname, std::string pesel) { //uzupelnic atrybuty
+    rc = sqlite3_open("warehouse.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    }
+    else {
+        fprintf(stdout, "Opened database successfully\n");
+    }
+
+
+    sql_string = "INSERT INTO EMPLOYERS (name,surname,pesel) VALUES ('" + name + "', '" + surname + "', " + pesel + "); ";
+
+    sql = sql_string.c_str();
+
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else {
+        std::cout << "Records created successfully\n";
+    }
+    sqlite3_close(db);
 }
 
-void 
 
+void addClientToDB(std::string name, std::string surname, std::string pesel) { //uzupelnic atrybuty
+    rc = sqlite3_open("warehouse.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    }
+    else {
+        fprintf(stdout, "Opened database successfully\n");
+    }
+
+
+    sql_string = "INSERT INTO CLIENTS (name,surname,pesel) VALUES ('" + name + "', '" + surname + "', " + pesel + "); ";
+
+    sql = sql_string.c_str();
+
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else {
+        std::cout << "Records created successfully\n";
+    }
+    sqlite3_close(db);
+}
+
+void addUserToDB(std::string login, std::string password) { //uzupelnic atrybuty
+    rc = sqlite3_open("warehouse.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    }
+    else {
+        fprintf(stdout, "Opened database successfully\n");
+    }
+
+
+    sql_string = "INSERT INTO USERS (login, password) VALUES ('" + login + "', '" + password + "'); ";
+
+    sql = sql_string.c_str();
+
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else {
+        std::cout << "Records created successfully\n";
+    }
+    sqlite3_close(db);
+}
