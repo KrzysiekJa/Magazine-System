8d6f0110bbe08e1fad55c35458b31dde57c9ce18
PiotrIzw <piotr.izworski.0@gmail.com>
2020-01-17 16:28:45 +0100

    Adding methods using database to classes

--- WarehouseSystem/Seller.cpp
+++ WarehouseSystem/Seller.cpp
@@ -3,13 +3,51 @@
 #include "Order.h"
 #include "Receipt.h"
 
-
 Seller::Seller(std::string Nname, std::string Nsurmane, std::string Nphone_number, int Nid, std::string Nbirth_date, int Npesel): 
 			Employee(Nname, Nsurmane, Nphone_number, Nid, Nbirth_date, Npesel) {}
 
-Order Seller::createOrder(Order order) {
-	// TODO - implement Saller::createOrder
-	throw "Not yet implemented";
+void Database::createTable() {
+	/*Create new tables in database*/
+	if (checkConnection()) {
+
+		/* Create SQL statement */
+		sql = "CREATE TABLE ORDERS("  \
+			"ID INTEGER PRIMARY KEY     NOT NULL," \
+			"PRODUCTNAME       TEXT    NOT NULL," \
+			"PRODUCTAMOUNT     TEXT    NOT NULL);";
+
+		/* Execute SQL statement */
+		rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else {
+			fprintf(stdout, "Table created successfully\n");
+		}
+		sqlite3_close(db);
+	}
+}
+
+void Seller::createOrder(std::string product_name, std::string product_amount) {
+	//przy dzidziczeniu wiecej niz jedna klasa nie dziala dziedziczenie database?
+	if (checkConnection()) {
+		sql_string = "INSERT INTO ORDERS (PRODUCTNAME, PRODUCTAMOUNT) VALUES ('" + product_name + "', " + product_amount + ""); ";
+
+		sql = sql_string.c_str();
+
+		rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else {
+			fprintf(stdout, "Records created successfully\n");
+		}
+		sqlite3_close(db);
+	}
 }
 
 Receipt Seller::sendReceipt(std::string text) {
--- WarehouseSystem/Seller.h
+++ WarehouseSystem/Seller.h
@@ -1,18 +1,20 @@
 #ifndef SALLER_H
 #define SALLER_H
+#include "Database.h"
 #include <string>
 #include "Employee.h"
 #include "OrderCreationInteface.h"
 
 
-class Seller : Employee, OrderCreationInteface {
+
+class Seller : Employee, OrderCreationInteface, Database {
 public:
 	Seller(std::string Nname, std::string Nsurmane, std::string Nphone_number, int Nid, std::string Nbirth_date, int Npesel);
 
-private:
 	Receipt receipt;
 	
-	Order createOrder(Order order);
+private:
+	void createOrder(std::string product_name, std::string product_amount);
 
 	Receipt sendReceipt(std::string text);
 
