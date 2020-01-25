e597e957a62532c933acaeeaa4e5628edadaf72a
PiotrIzw <piotr.izworski.0@gmail.com>
2020-01-25 18:18:10 +0100

    OrderCreationInterface class, header file implementation

--- Warehouse-System/OrderCreationInterface.cpp
+++ Warehouse-System/OrderCreationInterface.cpp
@@ -1,19 +1,84 @@
 #include "OrderCreationInterface.h"
 #include "Database.h"
 
-/* do zrobienia
-Order OrderCreationInteface::createOrder(Order order) {
-	// TODO - implement OrderCreationInteface::createOrder
-	throw "Not yet implemented";
+
+void OrderCreationInteface::createOrder(std::string client_id, std::string status) {
+	
+    if (checkConnection()) {
+        sql_string = "INSERT INTO ORDERS (CLIENT_ID, STATUS) VALUES (" + client_id + ", '" + status + "'); ";
+
+        sql = sql_string.c_str();
+
+        rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }
+        else {
+            std::cout << "Records created successfully\n";
+        }
+        sqlite3_close(db);
+    }
 }
 
-Product OrderCreationInteface::addProduct(Product product) {
-	// TODO - implement OrderCreationInteface::addProduct
-	throw "Not yet implemented";
+void OrderCreationInteface::addProduct(std::string name, std::string amount, std::string order_id, std::string status) {
+	
+    if (checkConnection()) {
+        sql_string = "INSERT INTO PRODUCTS (NAME, AMOUNT, ORDER_ID, STATUS) VALUES ('" + name + "', " + amount + ", " + order_id + ", '" + status + "'); ";
+
+        sql = sql_string.c_str();
+
+        rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }
+        else {
+            std::cout << "Records created successfully\n";
+        }
+        sqlite3_close(db);
+    }
 }
 
-Order OrderCreationInteface::sendOrder(Order order) {
-	// TODO - implement OrderCreationInteface::sendOrder
-	throw "Not yet implemented";
+void OrderCreationInteface::addItem(std::string name, std::string amount) {
+
+    if (checkConnection()) {
+        sql_string = "INSERT INTO ITEMS (NAME, AMOUNT=) VALUES ('" + name + "', " + amount + "); ";
+
+        sql = sql_string.c_str();
+
+        rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }
+        else {
+            std::cout << "Records created successfully\n";
+        }
+        sqlite3_close(db);
+    }
 }
-*/
+
+void OrderCreationInteface::sendOrder(std::string id) {
+	
+    if (checkConnection()) {
+        sql_string = "UPDATE ORDERS SET STATUS = ' in delivery' WHERE ID = " + id + "";
+        sql = sql_string.c_str();
+
+
+        rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }
+        else {
+            fprintf(stdout, "Operation done successfully\n");
+        }
+    }
+    sqlite3_close(db);
+
+}
--- Warehouse-System/OrderCreationInterface.h
+++ Warehouse-System/OrderCreationInterface.h
@@ -7,8 +7,9 @@
 class OrderCreationInteface : public Database {
 
 public:
-	void createOrder(std::string order);
+	void createOrder(std::string client_id, std::string status);
 
-	void addProduct(std::string product);
-	void sendOrder(std::string order);
+	void addProduct(std::string name, std::string amount, std::string order_id, std::string status);
+	void sendOrder(std::string id);
+	void addItem(std::string name, std::string amount);
 };
